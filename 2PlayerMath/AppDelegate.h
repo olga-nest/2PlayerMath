ab873443670fcf3b1e3d817583e5e3ae78b51d3d
olga-nest <olga.w.nesterova@gmail.com>
2017-10-09 18:40:26 -0700

    Initial Commit

---
+++ 2PlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2PlayerMath
+//
+//  Created by Olga on 10/9/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2PlayerMath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2PlayerMath
+//
+//  Created by Olga on 10/9/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
